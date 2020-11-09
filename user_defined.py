f510322e505d0df228cae648b9119d68c668715c
AadilVarsh <aadilvarshofficial@gmail.com>
2020-11-09 11:20:24 +0530

    Add files via upload

---
+++ user_defined.py
@@ -0,0 +1,18 @@
+
+
+#discord_token
+TOKEN = 'Nzc0MjA4OTczNzUwMTQwOTU5.X6UcSw.vghWAXhoYo9pJ2sKOuh9JhW2VPQ'
+#CHROMEDRIVER PATH
+PATH = 'C:\\chromedriver.exe'
+#wb name
+wb_name = 'tt.xlsx'
+#tr_names
+tr_maths = 'Sujisha Tp'
+tr_english = 'Malar Vasisht'
+tr_chemistry = 'Jijimolepkd Jijimolepkd'
+tr_biology = 'Sobha Rany'
+tr_physics = 'DIVYA PT'
+tr_hindi = 'sumitha pk'
+tr_history = 'Sushama Raghu'
+tr_geography = 'BENAZEER MA'
+tr_names = [tr_maths,tr_english,tr_chemistry,tr_biology,tr_physics,tr_hindi,tr_history,tr_geography]
