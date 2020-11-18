05d860f2c081a5261374980bdaf6a0d6ee4de375
AadilVarsh <aadilvarshofficial@gmail.com>
2020-11-18 20:10:19 +0530

    Update get_host.py

--- get_host.py
+++ get_host.py
@@ -1,7 +1,5 @@
-from googlesearch import search
-results = []
-query = 'what is my ip?'
+import urllib.request
 
-for i in search(query, num_results=3):
-  results.append(i)
-  print(i)
+external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+
+print(external_ip)
