99be0ac0ad61c23090f8ca1a9b15535f7c65cd68
AadilVarsh <aadilvarshofficial@gmail.com>
2020-11-18 20:14:50 +0530

    Update get_host.py

--- get_host.py
+++ get_host.py
@@ -1,5 +1,17 @@
-import urllib.request
+import socket
 
-external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+HOST = '52.54.118.98'  # Standard loopback interface address (localhost)
+PORT = 7171        # Port to listen on (non-privileged ports are > 1023)
 
-print(external_ip)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print('listening..')
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
