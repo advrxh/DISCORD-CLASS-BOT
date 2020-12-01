04c7af29ec1b41919fdc0ca2ae85bea1f73f647a
AadilVarsh <aadilvarshofficial@gmail.com>
2020-12-01 10:41:59 +0530

    Add files via upload

---
+++ quotes.py
@@ -0,0 +1,29 @@
+import json
+from random import choice
+
+with open('quotes.json', 'r+', encoding = 'utf-8') as f:
+	data = json.load(f)
+
+rand_no = list(range(0, 101))
+
+def quote(list_, data):
+	cur_choice = choice(list_)
+	return (data['quotes'][cur_choice]['quote'], data['quotes'][cur_choice]['author'])
+
+
+print(quote(rand_no, data))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
